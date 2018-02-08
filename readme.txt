1e8a09f51161fb8a3ab71f3e4635354ab4390c0f
Eyrane <1069680247@qq.com>
2018-02-08 15:05:15 +0800

    first commit

---
+++ readme.txt
@@ -0,0 +1,20 @@
+学习网址：http://www.runoob.com/python/python-operators.html
+
+若运行的时候出现如下错误：
+bash: ./test: /usr/bin/python^M: 解释器错误: 没有那个文件或目录  //文件编辑格式出错
+vim filename
+:set ff
+:set ff=unix
+
+test1包括了基础语法和变量类型（字符串、列表、元组、字典）
+
+test2包括了运算符（算术、比较、位、赋值、逻辑、成员、身份）和条件语句(if elif else)、循环语句(while for)
+
+test3包括列表的一些基本用法，如pop(),append(),len()等;以及元组和字典的基本用法
+
+test4时间和日期:time模块、calendar模块、datetime模块
+
+test5函数:函数的定义和调用、形参和实参、不定长参数、匿名函数
+
+test6.py:自定义的一个模块
+test6：对自定义的模块test6进行调用
---
+++ test6.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+内置模块、自定义模块、第三方模块
+'''
+#本文件自定义一个模块test6，该模块必须以.py为文件名，调用模块时要import test6
+
+
+'module test' #任何模块代码的第一个字符串都被视为模块的文档注释
+_author_ = 'Eyrane'  #_author_声明作者
+
+#以下为模块的代码
+import sys
+def test():
+    args = sys.argv
+    if len(args)==1:
+        print 'Hello, world!'
+    elif len(args)==2:
+        print 'Hello, %s!' % args[1]
+    else:
+        print 'Too many arguments!'
+
+def add(a, b):
+	return a+b
+
+
+#在命令行运行本模块文件时，Python解释器把一个特殊变量__name__置为__main__，
+#而如果在其他地方导入该模块时，if判断将失败
+#即直接运行本文件时，if成立；把本文件当中模块使用时，if不成立
+if __name__=='__main__':  
+	test()
+	print add(2,3)
+	
+def printadd( a ):
+	a+=[1,2,3]
+	return a
